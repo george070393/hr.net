0c39817daa40ec1eeb1c7b13a0511c9589eea610
Victor Grigoriu <vgrigoriu@gmail.com>
2015-04-22 14:56:30 +0300

    Job & location

---
+++ Personal.Entities/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace Personal.Entities
+{
+    public class Job
+    {
+        string JobId { get; set; }
+        string JobTitle { get; set; }
+        int MinSalary { get; set; }
+        int MaxSalary { get; set; }
+    }
+}
---
+++ Personal.Entities/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace Personal.Entities
+{
+    public class Location
+    {
+        int LocationId { get; set; }
+        string StreetAddress { get; set; }
+        string PostalCode { get; set; }
+        string City { get; set; }
+        string StateProvince { get; set; }
+    }
+}
