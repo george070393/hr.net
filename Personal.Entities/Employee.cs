5e923a2d6a7f191d619bb6435973ac1b1afd1ced
Victor Grigoriu <vgrigoriu@gmail.com>
2015-04-22 16:28:55 +0300

    Add Department, Employee

---
+++ Personal.Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Personal.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public Job Job { get; set; }
+        public decimal Salary { get; set; }
+        public decimal CommisionPercent { get; set; }
+        public Employee Manager { get; set; }
+        public Department Department { get; set; }
+    }
+}
