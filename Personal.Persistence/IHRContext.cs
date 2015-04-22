63f77e41f9e8d443cea72491215d71de7dc384f4
Victor Grigoriu <vgrigoriu@gmail.com>
2015-04-22 18:28:44 +0300

    Add DbContext interface

---
+++ Personal.Persistence/IHRContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Personal.Entities;
+
+namespace Personal.Persistence
+{
+    interface IHrContext
+    {
+        DbSet<Job> Jobs { get; }
+        DbSet<Location> Locations { get; }
+        DbSet<Department> Departments { get; }
+        DbSet<Employee> Employees { get; }
+
+        int SaveChanges();
+    }
+}
