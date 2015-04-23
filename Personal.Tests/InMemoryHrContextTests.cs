8d5cee18bbffa85a1739fff5147d85cb6c6c1e53
Victor Grigoriu <vgrigoriu@gmail.com>
2015-04-23 10:59:34 +0300

    Add test project

---
+++ Personal.Tests/InMemoryHrContextTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Personal.Entities;
+using Personal.Persistence;
+using Shouldly;
+
+namespace Personal.Tests
+{
+    public class InMemoryHrContextTests
+    {
+        public void CanAddAndRetrieveJob()
+        {
+            IHrContext context = new InMemoryHrContext();
+
+            var job = new Job
+            {
+                JobId = "CEO",
+                JobTitle = "Chief Executive Officer",
+                MinSalary = 1,
+                MaxSalary = int.MaxValue
+            };
+            context.Jobs.Add(job);
+
+            context.SaveChanges();
+
+            var retrievedJob = context.Jobs.Single(j => j.JobId == "CEO");
+            retrievedJob.ShouldBe(job);
+        }
+    }
+}
