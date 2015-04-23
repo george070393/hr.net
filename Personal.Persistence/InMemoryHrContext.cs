83df9ec75fd000a2d28e62ab97428d69beed81d8
Victor Grigoriu <vgrigoriu@gmail.com>
2015-04-23 10:41:06 +0300

    Implement InMemoryHrContext

---
+++ Personal.Persistence/InMemoryHrContext.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using Personal.Entities;
+
+namespace Personal.Persistence
+{
+    public class InMemoryHrContext : IHrContext
+    {
+        private readonly DbSet<Job> jobs;
+        private readonly DbSet<Location> locations;
+        private readonly DbSet<Department> departments;
+        private readonly DbSet<Employee> employees;
+
+        public DbSet<Job> Jobs
+        {
+            get { return jobs; }
+        }
+
+        public DbSet<Location> Locations
+        {
+            get { return locations; }
+        }
+
+        public DbSet<Department> Departments
+        {
+            get { return departments; }
+        }
+
+        public DbSet<Employee> Employees
+        {
+            get { return employees; }
+        }
+
+        public InMemoryHrContext()
+        {
+            jobs = new InMemoryDbSet<Job>();
+            locations = new InMemoryDbSet<Location>();
+            departments = new InMemoryDbSet<Department>();
+            employees = new InMemoryDbSet<Employee>();
+        }
+
+        public int SaveChanges()
+        {
+            return 1;
+        }
+    }
+}
